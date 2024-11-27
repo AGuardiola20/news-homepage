8792ccade339c08f864caa09eb49a696fdbd70ca
AGuardiola20 <andres.pguardiola@gmail.com>
2024-11-27 06:28:06 -0600

    add: Btn hover

--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -3,10 +3,17 @@ import styled from "styled-components";
 const ButtonContainer = styled.button`
   background-color: ${({ theme }) => theme.colors.softRed};
   border: none;
+  cursor: pointer;
   padding: 10px 40px;
   font-size: 1rem;
   font-weight: 600;
   text-transform: uppercase;
+  transition: 0.2s ease-in;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.offWhite};
+    background-color: ${({ theme }) => theme.colors.darkBlue};
+  }
 `;
 
 type PropsBtn = {
